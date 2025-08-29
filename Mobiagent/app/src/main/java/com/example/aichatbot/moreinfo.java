335262264bf1e5b02a04f65ca0f7e8eb6a89b312
PhantomPhoenix <logic_1729@sjtu.edu.cn>
2025-08-29 19:49:14 +0800

    feat: Initial commit for an application

---
+++ Mobiagent/app/src/main/java/com/example/aichatbot/moreinfo.java
@@ -0,0 +1,16 @@
+package com.mobi.agent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mobi.agent.R;
+
+public class moreinfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moreinfo);
+    }
+}
